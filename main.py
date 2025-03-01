6c2ffb291aa72f57ef49004c28da52dd12c9498e
Denis <den12262@mail.ru>
2025-03-01 10:18:52 +0300

    arrays: I redid it through the sets

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        if len(set(nums)) == len(nums):
-            return False
-        else:
-            return True
+        sets = set()
+        for elem in nums:
+            if elem in sets:
+                return True
+            else:
+                sets.add(elem)
+        return False
 
 
 nums_1 = [1, 2, 3, 4]
@@ -12,5 +15,5 @@ nums_2 = [1, 2, 2, 3, 4]
 
 solution = Solution()
 
-print(solution.containsDuplicate(nums_1))
+print(solution.containsDuplicate(nums_1))   
 print(solution.containsDuplicate(nums_2))
