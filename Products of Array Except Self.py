ddd1cd3f0f743efb47b74f04129ec9c166de4291
Denis <den12262@mail.ru>
2025-03-02 18:33:07 +0300

    arrays-7: first des

---
+++ Products of Array Except Self.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        result = []
+        const = 1
+        for inx_1 in range(len(nums)):
+            for inx_2 in range(len(nums)):
+                if inx_2 != inx_1:
+                    const *= nums[inx_2]
+            result.append(const)
+            const = 1
+        return result
+
+settings = Solution()
+
+nums = [1,2,4,6]
+
+print (settings.productExceptSelf(nums))
