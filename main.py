d6e1be4babe882b2684936d0c684ca7f0b7f1da8
Denis <den12262@mail.ru>
2025-03-02 13:55:40 +0300

    ruff

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from collections import defaultdict, Counter
 
+
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         anagrams = defaultdict(list)
 
         for elem in strs:
-            sort_s = ''.join(sorted(elem))
+            sort_s = "".join(sorted(elem))
             anagrams[sort_s].append(elem)
-        
+
         return list(anagrams.values())
-    
+
     def containsDuplicate(self, nums):
         numbers = set()
         for elem in numbers:
