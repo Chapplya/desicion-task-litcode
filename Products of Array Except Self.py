6623f5e49125eff3015454d4c47e92c4cc1259be
Denis <den12262@mail.ru>
2025-03-04 20:21:00 +0300

    arrays-7: ruff

--- Products of Array Except Self.py
+++ Products of Array Except Self.py
@@ -3,17 +3,19 @@ class Solution:
         prod, zero_cnt = 1, 0
         for num in nums:
             if num:
-                print (num)
+                print(num)
                 prod *= num
             else:
-                zero_cnt +=  1
-        if zero_cnt > 1: return [0] * len(nums)
+                zero_cnt += 1
+        if zero_cnt > 1:
+            return [0] * len(nums)
 
         res = [0] * len(nums)
         for i, c in enumerate(nums):
-            if zero_cnt: 
+            if zero_cnt:
                 res[i] = 0 if c != 0 else prod
-            else: res[i] = prod // c
+            else:
+                res[i] = prod // c
         return res
 
 
