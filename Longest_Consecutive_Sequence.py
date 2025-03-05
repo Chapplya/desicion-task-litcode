3765fa61d5e3f2d41d57e3896bcf9a0fbbf127d0
Denis <den12262@mail.ru>
2025-03-05 09:11:33 +0300

    arrays-9: super solution

--- Longest_Consecutive_Sequence.py
+++ Longest_Consecutive_Sequence.py
@@ -2,35 +2,22 @@ class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
         if nums == []:
             return 0
-        set_num = set()
-        nums = sorted(nums)
-        print(nums)
-        mas = []
-        for i in range(len(nums)):
-            if set_num == set():
-                set_num.add(nums[i])
-                # print(set_num)
-                continue
-            if nums[i] or nums[i] == 0:
-                if nums[i - 1] == nums[i]:
-                    continue
-                if nums[i - 1] - nums[i] == -1:
-                    set_num.add(nums[i])
-                    print(set_num)
-                else:
-                    mas.append(len(set_num))
-                    set_num = set()
-                    set_num.add(nums[i])
-                    continue
-            else:
-                mas.append(len(set_num))
-                return max(mas.append(len(set_num)))
-        mas.append(len(set_num))
-        return max(mas)
+
+        set_nums = set(nums)
+
+        longest = 0
+
+        for elem in set_nums:
+            if elem - 1 not in set_nums:
+                lenght = 1
+                while elem + lenght in set_nums:
+                    lenght += 1
+                longest = max(lenght, longest)
+        return longest
 
 
 settings = Solution()
 
-nums = [0, 3, 2, 5, 4, 6, 1, 1]
-print(nums.sort)
+nums = [-1, 0, 3, 2, 5, 4, 6, 1, 1]
+
 print(settings.longestConsecutive(nums))
